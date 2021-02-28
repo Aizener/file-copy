8067338e0919f65ac1eb30bb54b66452d216779b
Aizener <1215627787@qq.com>
2021-02-28 21:24:48 +0800

    feat: #master 解决昨天解决失败的问题，优化异常捕获

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 ### Get Started
 
+> 一个基于Node.js拷贝文件的工具
+
 ### 更新
-- 2021-02-27: 把文件读写流改用`async/await`的方式同步执行，虽然效率变慢，但能解决报错：`Error: EMFILE, too many open files`；
-- 2021-02-27: 修改git全局用户信息；
+- 2021-02-27: 把文件读写流改用`async/await`的方式同步执行，虽然效率变慢，~~但能解决报错：`Error: EMFILE, too many open files`~~；
+
+  2021-02-27: 修改git全局用户信息；
+
+- 2021-02-28：重新调整`async/await`的使用，这才解决`Error: EMFILE, too many open files`这个问题，但是发现速度明显变慢，而且同步性能确实较差，9200多个文件大概100M的目录，拷贝了一两分钟，估计还得看计算机的配置...
 
 ```git
 git@github.com:Aizener/file-copy.git
@@ -17,11 +22,11 @@ node index.js
 or
 
 ```shell
-node index.js 1|0
+node index.js 1|0  // 1表示是否新建对应目录，0则不是，默认0
 ```
 
 or
 
 ```shell
-node index.js 1|0 ./targetDir
+node index.js 1|0 ./targetDir	// 拷贝当前目录指定的某个目录
 ```
--- index.js
+++ index.js
@@ -4,47 +4,60 @@ const path = require('path')
 let [dir = 0, dirPath = "./"] = process.argv.slice(2)
 dir = Boolean(+dir)
 
-const targetDirPath = path.join('./out', dirPath)
+const baseDir = './out5'
+const targetDirPath = path.join(baseDir, dirPath)
 
 if (dir) {
   mkdirs(targetDirPath)
 } else {
   if (!isExsist(targetDirPath)) {
-    fs.mkdirSync('./out')
+    fs.mkdirSync(baseDir)
   }
 }
 
-function copyFile (dirPath) {
+// 函数实现
+async function copyFile (dirPath) {
   const res = fs.readdirSync(dirPath)
   for (let item of res) {
-    (async () => {
-      // 获取当前相对路径
-      const targetPath = path.join(dirPath, item)
-      // 定义输出目标路径，dir表示终端传入的值（0为不生成对应文件夹）
-      const wsPath = path.join('./out', dir ? targetPath : item)
-      // 获取当前对象信息
-      const target = fs.statSync(targetPath)
-      if (target.isDirectory()) {
-        if (!isExsist(wsPath) && item !== 'out') {
-          dir && fs.mkdirSync(wsPath) // 创建对应文件夹
-          copyFile(targetPath)
-        }
-      } else {
-        if (item !== 'index.js') {
-          // 创建管道写入数据
+    // 获取当前相对路径
+    const targetPath = path.join(dirPath, item)
+    // 定义输出目标路径，dir表示终端传入的值（0为不生成对应文件夹）
+    const wsPath = path.join(baseDir, dir ? targetPath : item)
+    // 获取当前对象信息
+    let target = null
+    try {
+      target = fs.statSync(targetPath)
+    } catch (err) {
+      console.warn(err)
+    }
+    if (target && target.isDirectory()) {
+      if (!isExsist(wsPath) && item !== 'out') {
+        dir && fs.mkdirSync(wsPath) // 创建对应文件夹
+        copyFile(targetPath)
+      }
+    } else {
+      if (item !== 'index.js') {
+        // 创建管道写入数据
+        try {
           await write(wsPath, targetPath)
+        } catch (err) {
+          console.warn(err)
         }
       }
-    })()
+    }
   }
 }
 
 // 改为同步的方式读写，解决报错：Error: EMFILE, too many open files
-async function write (wsPath, targetPath) {
-  const ws = fs.createWriteStream(wsPath)
-  fs.createReadStream(targetPath).pipe(ws)
-  ws.on('close', () => {
-    console.log(targetPath + '下载完成...')
+function write (wsPath, targetPath) {
+  return new Promise(resolve => {
+    const ws = fs.createWriteStream(wsPath)
+    fs.createReadStream(targetPath).pipe(ws)
+    ws.on('close', () => {
+      console.log(targetPath + '拷贝完成...')
+      ws.end()
+      resolve()
+    })
   })
 }
 
